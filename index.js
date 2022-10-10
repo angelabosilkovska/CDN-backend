1076108408d2eb1f611af035437934b92b7039e2
Angela Bosilkovska <angela.bosilkovska@uklo.edu.mk>
2022-10-10 09:28:13 +0200

    multiple responses based on app key

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const baseUrl = "http://192.168.1.105:3000";
+const baseUrl = "http://192.168.0.104:3000";
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
-app.get("/get-endpoints", (req, res) => {
-  console.log("getting endpoints");
-  res.json({
-    reportingURL: `${baseUrl}/sendreports`,
-    servers: [
-      { id: 0, name: "cdn1", weight: 60, price: 20, url: `${baseUrl}/first` },
-      { id: 1, name: "cdn2", weight: 30, price: 60, url: `${baseUrl}/second` },
-      { id: 2, name: "cdn3", weight: 10, price: 50, url: `${baseUrl}/third` },
-      { id: 3, name: "cdn4", weight: 10, price: 100, url: `${baseUrl}/forth` },
-    ],
-  });
+
+app.get("/get-endpoints/:appKey", (req, res) => {
+  console.log("getting endpoints",req.params.appKey);
+  if(req.params.appKey == 'oHlEsC7CD3dwV1DZBcZ9WzJx8wsjAZf7'){
+    res.json({
+      reportingURL: `${baseUrl}/sendreports`,
+      servers: [
+        { id: 0, name: "cdn1", weight: 60, price: 20, url: `${baseUrl}/first` },
+        { id: 2, name: "cdn3", weight: 10, price: 50, url: `${baseUrl}/third` },
+        { id: 3, name: "cdn4", weight: 10, price: 100, url: `${baseUrl}/forth` },
+        { id: 1, name: "cdn5", weight: 30, price: 80, url: `${baseUrl}/fifth` },
+      ],
+    });
+  }else if(req.params.appKey == 'key2'){
+    res.json({
+      reportingURL: `${baseUrl}/sendreports`,
+      servers: [
+        { id: 0, name: "cdn1", weight: 60, price: 20, url: `${baseUrl}/first` },
+        { id: 1, name: "cdn2", weight: 30, price: 60, url: `${baseUrl}/second` },
+        { id: 2, name: "cdn3", weight: 10, price: 50, url: `${baseUrl}/third` },
+      ],
+    });
+  }
+  else{
+    res.status(401).json('invalid app key')
+  }
+
 });
 
 app.post("/sendreports", (req, res) => {
@@ -30,13 +46,16 @@ app.get("/first", (req, res) => {
   setTimeout(() => res.json({ success: true }), 2000);
 });
 app.get("/second", (req, res) => {
-  setTimeout(() => res.json({ success: true }), 500);
+  setTimeout(() => res.sendStatus(400), 500);
 });
 app.get("/third", (req, res) => {
   setTimeout(() => res.json({ success: true }), 1000);
 });
 app.get("/forth", (req, res) => {
   setTimeout(() => res.json({ success: true }), 100);
+});
+app.get("/fifth", (req, res) => {
+  setTimeout(() => res.json({ success: true }), 200);
 });
 
 app.listen(port, () => {
